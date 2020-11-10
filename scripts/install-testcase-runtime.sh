87f942f901f565d80ead8c4358eab9072cc0fdf8
Lokesh Kumar Goel <lokeshkumar.goel@lge.com>
2020-11-09 16:37:40 -0800

    AUTO-5263: Add visualEditor to set of supported simulations

--- scripts/install-testcase-runtime.sh
+++ scripts/install-testcase-runtime.sh
@@ -10,7 +10,7 @@ function usage {
     exit 1
 }
 
-SUPPORTED_RUNTIMES='pythonAPI pythonApi scenic vse'
+SUPPORTED_RUNTIMES='pythonAPI pythonApi scenic vse visualEditor'
 WRAPPER_SCRIPT=scenario_runner.sh
 
 if [ ! -x ${SIMULATOR_DIR}/simulator ]; then
